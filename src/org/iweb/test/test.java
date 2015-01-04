5e97442abc5edf5ed3d472f591735740e071acfe
kreocn <kreo@qq.com>
2015-01-04 11:36:53 +0800

    fix mutipart dept

--- src/org/iweb/test/test.java
+++ src/org/iweb/test/test.java
@@ -12,19 +12,9 @@ public class test {
 	private StringBuffer sql = new StringBuffer();
 
 	public test() {
-		String yearMonth = "2014-9";
-		String deptGroup = "21";
-		sql.append("SELECT a.`apply_dept`,d.`dept_cname`,COUNT(*) cout").append(" FROM qkjm_h_process p,qkjm_r_active a,s_sys_department d").append(" WHERE p.`process_id` = 1")
-				.append(" AND p.`biz_id` = a.`uuid`").append("AND p.`biz_sign` = 'ACTIVE_APPLY_PASS'").append("AND p.`biz_time` > DATE_ADD(CONCAT(").append("'").append(yearMonth)
-				.append("'").append(",'-26'), INTERVAL -1 MONTH)").append("AND CONCAT(").append("'").append(yearMonth).append("'").append(",'-26')")
-				.append("AND a.`apply_dept` = d.`dept_code`").append("AND a.`apply_dept` LIKE '").append(deptGroup).append("%'").append("GROUP BY a.`apply_dept`")
-				.append("UNION ALL").append(" SELECT b.apply_dept,rd.dept_cname,b.cout FROM (").append("SELECT SUBSTR(a.`apply_dept`,1,3) `apply_dept`,COUNT(*) cout")
-				.append(" FROM qkjm_h_process p,qkjm_r_active a,s_sys_department d").append(" WHERE p.`process_id` = 1").append(" AND p.`biz_id` = a.`uuid`")
-				.append("AND p.`biz_sign` = 'ACTIVE_APPLY_PASS'").append("AND p.`biz_time` > DATE_ADD(CONCAT(").append("'").append(yearMonth).append("'")
-				.append(",'-26'), INTERVAL -1 MONTH)").append("AND CONCAT(").append("'").append(yearMonth).append("'").append(",'-26')")
-				.append("AND a.`apply_dept` = d.`dept_code`").append("AND a.`apply_dept` LIKE '").append(deptGroup).append("%'").append("GROUP BY SUBSTR(a.`apply_dept`,1,3)")
-				.append(") b,s_sys_department rd").append(" WHERE b.apply_dept = rd.dept_code");
-		System.out.println(sql.toString());
+		String as = "0.1##0.2##0.3";
+		String[] as2 = as.split("##");
+		System.out.println(as2[1]);
 	}
 
 	/**
