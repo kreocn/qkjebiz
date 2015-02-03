d6a23ed37704bcd273e45a2ecf9833b145364e6b
sunshanshan <sunshanshan@51qkj.com>
2015-02-03 15:24:54 +0800

    tt

--- src/com/aqkj/adm/text/check.java
+++ src/com/aqkj/adm/text/check.java
@@ -1,5 +1,5 @@
 package com.aqkj.adm.text;
 
 public class check {
-
+	private int SDStatus30=30;//大区通过
 }
--- src/org/iweb/test/test.java
+++ src/org/iweb/test/test.java
@@ -1,5 +1,11 @@
 package org.iweb.test;
 
+import org.iweb.sys.JSONUtil;
+import org.iweb.sys.ToolsUtil;
+import org.iweb.sys.cache.CacheFactory;
+import org.iweb.sys.cache.SysCacheLogic;
+import org.iweb.sys.cache.SysDBCacheLogic;
+
 public class test {
 	private String str = "SELECT c.COLUMN_NAME,c.COLUMN_DEFAULT,c.IS_NULLABLE,c.DATA_TYPE," + "c.NUMERIC_SCALE,c.COLUMN_COMMENT,	"
 			+ "IFNULL(c.CHARACTER_MAXIMUM_LENGTH,c.NUMERIC_PRECISION) AS $MAX_LENGTH$ " + "FROM information_schema.`COLUMNS` c WHERE c.TABLE_SCHEMA = 'AAA' "
@@ -8,9 +14,9 @@ public class test {
 	private StringBuffer sql = new StringBuffer();
 
 	public test() {
-		String s = "asdasdsad*";
-		System.out.println(s.substring(s.length() - 1));
-		System.out.println(s.substring(0, s.length() - 1));
+		String str = (String)CacheFactory.getCacheInstance().get(SysDBCacheLogic.CACHE_DEPT_PREFIX_SUB+"210");//返回210所有子部门josn
+		String[] s = (String[])JSONUtil.toObject(str, String[].class);//转换成数组
+		ToolsUtil.isIn("21011", s);//判断在不在数组中
 	}
 
 	/**
