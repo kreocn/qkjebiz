d238ae9ef5324b55ea900c9b3075e570d8e0848d
kreo <kreo@qq.com>
2015-03-11 14:16:24 +0800

    merge dev

--- src/com/qkj/manage/check/CheckSkip.java
+++ src/com/qkj/manage/check/CheckSkip.java
@@ -14,7 +14,22 @@ import com.qkj.manage.domain.Active;
 
 public class CheckSkip {
 	private SkipStep skipStep;
-	private List<SkipStep> skipSteps = new ArrayList<>();
+	private static List<SkipStep> skipSteps = new ArrayList<>();
+	static {
+		// 北京
+		skipSteps.add(new SkipStep("210", 1, 4, 7)); // 申请 4总监 7跳过销管副总
+		skipSteps.add(new SkipStep("210", 1, 14, 17));// 结案 14总监 17跳过大区
+		// 省外
+		skipSteps.add(new SkipStep("211", 1, 4, 7)); // 申请 4总监 7跳过销管副总
+		skipSteps.add(new SkipStep("211", 1, 14, 17));// 结案 14总监 17跳过大区
+		// 西藏
+		skipSteps.add(new SkipStep("2302", 1, 2, 3)); // 申请 2大区 3跳过销管经理
+		skipSteps.add(new SkipStep("2302", 1, 12, 13));// 结案 12大区 13跳过销管经理
+		// 新疆
+		skipSteps.add(new SkipStep("22030", 1, 2, 3)); // 申请 2大区 3跳过销管经理
+		skipSteps.add(new SkipStep("22030", 1, 12, 13));// 结案 12大区 13跳过销管经理
+	}
+
 	private Step step = new Step();
 	private static Active active;
 	private String str;
@@ -28,12 +43,12 @@ public class CheckSkip {
 		this.skipStep = skipStep;
 	}
 
-	public List<SkipStep> getSkipSteps() {
+	public static List<SkipStep> getSkipSteps() {
 		return skipSteps;
 	}
 
-	public void setSkipSteps(List<SkipStep> skipSteps) {
-		this.skipSteps = skipSteps;
+	public static void setSkipSteps(List<SkipStep> skipSteps) {
+		CheckSkip.skipSteps = skipSteps;
 	}
 
 	public Step getStep() {
@@ -56,17 +71,19 @@ public class CheckSkip {
 		this.setActive(active);
 		// check.getClass().getMethod(str, new Class[]{});
 		String userid = ContextHelper.getUserLoginUuid();
-		skipList();
+		// skipList();
 		int falg = 0;
 		if (skipSteps.size() > 0) {
-		/*	for (int i = 0; i < skipSteps.size(); i++) {
-				SkipStep as = new SkipStep();
-				as = skipSteps.get(i);
-				String str = (String) CacheFactory.getCacheInstance().get(SysDBCacheLogic.CACHE_DEPT_PREFIX_PARENT + active.getApply_dept());//
-				String[] s = (String[]) JSONUtil.toObject(str, String[].class);// 转换成数组
-				Boolean iskip = ToolsUtil.isIn(as.getSkip_dept(), s);// 判断在不在数组中
-			}*/
-			
+			/*
+			 * for (int i = 0; i < skipSteps.size(); i++) {
+			 * SkipStep as = new SkipStep();
+			 * as = skipSteps.get(i);
+			 * String str = (String) CacheFactory.getCacheInstance().get(SysDBCacheLogic.CACHE_DEPT_PREFIX_PARENT + active.getApply_dept());//
+			 * String[] s = (String[]) JSONUtil.toObject(str, String[].class);// 转换成数组
+			 * Boolean iskip = ToolsUtil.isIn(as.getSkip_dept(), s);// 判断在不在数组中
+			 * }
+			 */
+
 			for (int i = 0; i < skipSteps.size(); i++) {
 				SkipStep as = new SkipStep();
 				as = skipSteps.get(i);
@@ -74,15 +91,15 @@ public class CheckSkip {
 					String str = (String) CacheFactory.getCacheInstance().get(SysDBCacheLogic.CACHE_DEPT_PREFIX_PARENT + active.getApply_dept());//
 					String[] s = (String[]) JSONUtil.toObject(str, String[].class);// 转换成数组
 					Boolean iskip = ToolsUtil.isIn(as.getSkip_dept(), s);// 判断在不在数组中
-					if(as.getSkip_dept().equals(active.getApply_dept())){
-						iskip=true;
+					if (as.getSkip_dept().equals(active.getApply_dept())) {
+						iskip = true;
 					}
 					if (iskip == true) {// 是特殊部门
-						if (st ==as.getStart_step()) {
-						specialStep(st,as);
-						falg = 1;
-						break;
-						}else{
+						if (st == as.getStart_step()) {
+							specialStep(st, as);
+							falg = 1;
+							break;
+						} else {
 							continue;
 						}
 					} else {
@@ -90,11 +107,11 @@ public class CheckSkip {
 					}
 				} else {// 不包含子部门
 					if (active.getApply_dept().equals(as.getSkip_dept())) {// 是特殊部门
-						if (st ==as.getStart_step()) {
-						specialStep(st,as);
-						falg = 1;
-						break;
-						}else{
+						if (st == as.getStart_step()) {
+							specialStep(st, as);
+							falg = 1;
+							break;
+						} else {
 							continue;
 						}
 					} else {
@@ -110,22 +127,24 @@ public class CheckSkip {
 			normalStep(st);
 		}
 	}
-	
+
 	public void checkSkip2(Active active, int st) {
 		this.setActive(active);
 		// check.getClass().getMethod(str, new Class[]{});
 		String userid = ContextHelper.getUserLoginUuid();
-		skipList();
+		// skipList();
 		int falg = 0;
 		if (skipSteps.size() > 0) {
-		/*	for (int i = 0; i < skipSteps.size(); i++) {
-				SkipStep as = new SkipStep();
-				as = skipSteps.get(i);
-				String str = (String) CacheFactory.getCacheInstance().get(SysDBCacheLogic.CACHE_DEPT_PREFIX_PARENT + active.getApply_dept());//
-				String[] s = (String[]) JSONUtil.toObject(str, String[].class);// 转换成数组
-				Boolean iskip = ToolsUtil.isIn(as.getSkip_dept(), s);// 判断在不在数组中
-			}*/
-			
+			/*
+			 * for (int i = 0; i < skipSteps.size(); i++) {
+			 * SkipStep as = new SkipStep();
+			 * as = skipSteps.get(i);
+			 * String str = (String) CacheFactory.getCacheInstance().get(SysDBCacheLogic.CACHE_DEPT_PREFIX_PARENT + active.getApply_dept());//
+			 * String[] s = (String[]) JSONUtil.toObject(str, String[].class);// 转换成数组
+			 * Boolean iskip = ToolsUtil.isIn(as.getSkip_dept(), s);// 判断在不在数组中
+			 * }
+			 */
+
 			for (int i = 0; i < skipSteps.size(); i++) {
 				SkipStep as = new SkipStep();
 				as = skipSteps.get(i);
@@ -133,15 +152,15 @@ public class CheckSkip {
 					String str = (String) CacheFactory.getCacheInstance().get(SysDBCacheLogic.CACHE_DEPT_PREFIX_PARENT + active.getApply_dept());//
 					String[] s = (String[]) JSONUtil.toObject(str, String[].class);// 转换成数组
 					Boolean iskip = ToolsUtil.isIn(as.getSkip_dept(), s);// 判断在不在数组中
-					if(as.getSkip_dept().equals(active.getApply_dept())){
-						iskip=true;
+					if (as.getSkip_dept().equals(active.getApply_dept())) {
+						iskip = true;
 					}
 					if (iskip == true) {// 是特殊部门
-						if (st ==as.getStart_step()) {
-						specialStep2(st,as);
-						falg = 1;
-						break;
-						}else{
+						if (st == as.getStart_step()) {
+							specialStep2(st, as);
+							falg = 1;
+							break;
+						} else {
 							continue;
 						}
 					} else {
@@ -149,11 +168,11 @@ public class CheckSkip {
 					}
 				} else {// 不包含子部门
 					if (active.getApply_dept().equals(as.getSkip_dept())) {// 是特殊部门
-						if (st ==as.getStart_step()) {
-						specialStep(st,as);
-						falg = 1;
-						break;
-						}else{
+						if (st == as.getStart_step()) {
+							specialStep(st, as);
+							falg = 1;
+							break;
+						} else {
 							continue;
 						}
 					} else {
@@ -170,30 +189,30 @@ public class CheckSkip {
 		}
 	}
 
-	private void specialStep(int st,SkipStep as) {
+	private void specialStep(int st, SkipStep as) {
 		String userid = ContextHelper.getUserLoginUuid();
 		str = "step" + st;// 现在调用的步骤
 		skipstr = "step" + as.getSkip_step();// 跳过的步骤
-			try {
-				step.getClass().getMethod(str, new Class[] { String.class }).invoke(step, new Object[] { userid });
-				step.getClass().getMethod(skipstr, new Class[] { String.class }).invoke(step, new Object[] { "2" });// 跳过的方法
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			step.getClass().getMethod(str, new Class[] { String.class }).invoke(step, new Object[] { userid });
+			step.getClass().getMethod(skipstr, new Class[] { String.class }).invoke(step, new Object[] { "2" });// 跳过的方法
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	private void specialStep2(int st,SkipStep as) {
+
+	private void specialStep2(int st, SkipStep as) {
 		String userid = ContextHelper.getUserLoginUuid();
 		str = "step" + st;// 现在调用的步骤
 		skipstr = "step" + as.getSkip_step();// 跳过的步骤
-			try {
-				//step.getClass().getMethod(str, new Class[] { String.class }).invoke(step, new Object[] { userid });
-				step.getClass().getMethod(skipstr, new Class[] { String.class }).invoke(step, new Object[] { "2" });// 跳过的方法
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			// step.getClass().getMethod(str, new Class[] { String.class }).invoke(step, new Object[] { userid });
+			step.getClass().getMethod(skipstr, new Class[] { String.class }).invoke(step, new Object[] { "2" });// 跳过的方法
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private void normalStep(int st) {
@@ -205,64 +224,6 @@ public class CheckSkip {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	private void skipList() {
-		//北京+省外总监审后，跳过销管副总直接到财务
-		skipStep = new SkipStep();
-		skipStep.setIsSub(1);
-		skipStep.setSkip_dept("210");
-		skipStep.setStart_step(4);//总监
-		skipStep.setSkip_step(7);//申请跳过销管副总
-		skipSteps.add(skipStep);
-		skipStep = new SkipStep();
-		skipStep.setIsSub(1);
-		skipStep.setSkip_dept("210");
-		skipStep.setStart_step(14);//总监
-		skipStep.setSkip_step(17);//结案大区跳过
-		skipSteps.add(skipStep);
-		
-		skipStep = new SkipStep();
-		skipStep.setIsSub(1);
-		skipStep.setSkip_dept("211");
-		skipStep.setStart_step(4);//总监
-		skipStep.setSkip_step(7);//申请跳过销管副总
-		skipSteps.add(skipStep);
-		skipStep = new SkipStep();
-		skipStep.setIsSub(1);
-		skipStep.setSkip_dept("211");
-		skipStep.setStart_step(14);//总监
-		skipStep.setSkip_step(17);//结案大区跳过
-		skipSteps.add(skipStep);
-		
-		//新疆+西藏跳过销管经理
-		skipStep = new SkipStep();
-		skipStep.setIsSub(1);
-		skipStep.setSkip_dept("2302");//西藏
-		skipStep.setStart_step(2);//大区
-		skipStep.setSkip_step(3);//申请跳过销管经理
-		skipSteps.add(skipStep);
-		skipStep = new SkipStep();
-		skipStep.setIsSub(1);
-		skipStep.setSkip_dept("2302");
-		skipStep.setStart_step(12);
-		skipStep.setSkip_step(13);
-		skipSteps.add(skipStep);
-		
-		skipStep = new SkipStep();
-		skipStep.setIsSub(0);
-		skipStep.setSkip_dept("22030");//新疆
-		skipStep.setStart_step(2);//大区
-		skipStep.setSkip_step(3);//申请跳过销管经理
-		skipSteps.add(skipStep);
-		skipStep = new SkipStep();
-		skipStep.setIsSub(0);
-		skipStep.setSkip_dept("22030");
-		skipStep.setStart_step(12);
-		skipStep.setSkip_step(13);
-		skipSteps.add(skipStep);
-		
-		
 	}
 
 	public static void main(String[] args) throws Exception {
